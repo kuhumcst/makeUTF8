45f9999f37033753fdaa40cf9e39efd8ea4b4a04
Bart Jongejan <bart@cst.dk>
2015-10-14 13:24:27 +0200

    Replced FooBar by real name.

--- iso2unicode/iso2unicode.cpp
+++ iso2unicode/iso2unicode.cpp
@@ -10,13 +10,13 @@
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    makeUTF8 is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with makeUTF8.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <assert.h>
 #include <stdio.h>
--- makeISOn/main.cpp
+++ makeISOn/main.cpp
@@ -10,13 +10,13 @@
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Foobar is distributed in the hope that it will be useful,
+    makeISO is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with makeISO.  If not, see <http://www.gnu.org/licenses/>.
 */
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
